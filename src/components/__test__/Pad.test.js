949769c61185e4f1cda193c7457e3fe6ef234028
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-03-02 14:29:09 -0400

    Test components and event

--- src/components/__test__/Pad.test.js
+++ src/components/__test__/Pad.test.js
@@ -1,9 +1,18 @@
+import { render, screen, fireEvent } from '@testing-library/react';
 import renderer from 'react-test-renderer';
-import Pad from '../Pad';
+import CalcButton from '../Pad';
 
 describe('Pad Component', () => {
   test('Tree', () => {
-    const tree = renderer.create(<Pad />).toJSON();
+    const tree = renderer.create(<CalcButton symbol="1" function1={jest.fn()} />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  test('Click button', () => {
+    const handleClick = jest.fn();
+    render(<CalcButton symbol="1" function1={handleClick} />);
+    const button = screen.getByRole('button');
+    fireEvent.click(button, {});
+    expect(button.textContent).toBe('1');
   });
 });
--- src/logic/__test__/calculate.test.js
+++ src/logic/__test__/calculate.test.js
@@ -8,18 +8,18 @@ describe('Operate function', () => {
       operation: null,
     }, '4').next).toBe('4');
   });
-    test('Clears the output screen', () => {
+  test('Clears the output screen', () => {
     expect(calculate({
       total: '5',
       next: '23',
       operation: '+',
     }, 'AC').next).toBeNull();
   });
-    test('Displays a decimal number', () => {
+  test('Displays a decimal number', () => {
     expect(calculate({
       total: '5',
       next: '23',
       operation: '.',
     }, '.').next).toBe('23.');
   });
-})
+});
