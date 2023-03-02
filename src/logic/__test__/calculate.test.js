29867b102bc9d12ebe6bfd1cbfa1f5cf611c5843
dazehinn <dazehinn.yahoo.fr>
2023-03-02 18:50:36 +0100

    Add more tests to the calculator file

--- src/logic/__test__/calculate.test.js
+++ src/logic/__test__/calculate.test.js
@@ -7,5 +7,19 @@ describe('Operate function', () => {
       next: null,
       operation: null,
     }, '4').next).toBe('4');
-  })
+  });
+    test('Clears the output screen', () => {
+    expect(calculate({
+      total: '5',
+      next: '23',
+      operation: '+',
+    }, 'AC').next).toBeNull();
+  });
+    test('Displays a decimal number', () => {
+    expect(calculate({
+      total: '5',
+      next: '23',
+      operation: '.',
+    }, '.').next).toBe('23.');
+  });
 })
